8ec7237d61d50656db8d83dcb93c4aae7358b062
Kirubha7 <45505238+kirubha7@users.noreply.github.com>
2021-10-06 21:23:14 +0530

    Api File Updated
    Api File Updated

--- api/create.php
+++ api/create.php
@@ -12,6 +12,7 @@ if(!empty($postdata)){
   $sql = "INSERT INTO user (name, email, phone,website,contact_name,contact_email,contact_phone,notes,type,category,commision_percentage,active_form,critical_account,payment_option) values 
   ('".$postdata['name']."','".$postdata['email']."','".$postdata['phone']."','".$postdata['website']."','".$postdata['contact_name']."','".$postdata['contact_email']."','".$postdata['contact_phone']."','".$postdata['notes']."','".$postdata['type']."','".implode(',',$postdata['category'])."','".$postdata['commision_percentage']."','".$postdata['active_form']."','".$postdata['critical_account']."','".$postdata['payment_option']."')";
   $result = $conn->query($sql);
+  
   if ($result) {
     echo 1;exit;
   }
